913c66e4066e5dd6b09b47b0776652fbe981a64d
Bryan <git@singee.me>
2026-01-05 12:34:37 +0800

    init project

---
+++ src/routes/__root.tsx
@@ -0,0 +1,80 @@
+import {
+  HeadContent,
+  Scripts,
+  createRootRouteWithContext,
+} from '@tanstack/react-router';
+import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools';
+import { ReactQueryDevtoolsPanel } from '@tanstack/react-query-devtools';
+import { TanStackDevtools } from '@tanstack/react-devtools';
+import { MantineProvider } from '@mantine/core';
+import { Toaster } from 'sonner';
+import { NavigationProgress } from '@mantine/nprogress';
+import { ModalsProvider } from '@mantine/modals';
+import type { QueryClient } from '@tanstack/react-query';
+import appCss from '~styles.css?url';
+import { shadcnTheme as theme } from '~ui/shadcn-blue-theme/theme';
+import { shadcnCssVariableResolver as cssVariablesResolver } from '~ui/shadcn-blue-theme/cssVariableResolver';
+
+export const Route = createRootRouteWithContext<{
+  queryClient: QueryClient;
+}>()({
+  head: () => ({
+    meta: [
+      {
+        charSet: 'utf-8',
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1',
+      },
+      {
+        title: 'TanStack Start Starter',
+      },
+    ],
+    links: [
+      {
+        rel: 'stylesheet',
+        href: appCss,
+      },
+    ],
+  }),
+
+  shellComponent: RootDocument,
+});
+
+function RootDocument({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <head>
+        <HeadContent />
+      </head>
+      <body>
+        <MantineProvider
+          theme={theme}
+          cssVariablesResolver={cssVariablesResolver}
+        >
+          <ModalsProvider>{children}</ModalsProvider>
+
+          <Toaster position="top-center" richColors />
+          <NavigationProgress />
+        </MantineProvider>
+        <TanStackDevtools
+          config={{
+            position: 'bottom-right',
+          }}
+          plugins={[
+            {
+              name: 'Tanstack Router',
+              render: <TanStackRouterDevtoolsPanel />,
+            },
+            {
+              name: 'React Query',
+              render: <ReactQueryDevtoolsPanel />,
+            },
+          ]}
+        />
+        <Scripts />
+      </body>
+    </html>
+  );
+}
---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite';
+import { tanstackStart } from '@tanstack/react-start/plugin/vite';
+import viteReact from '@vitejs/plugin-react';
+import viteTsConfigPaths from 'vite-tsconfig-paths';
+import { nitro } from 'nitro/vite';
+import { devtools } from '@tanstack/devtools-vite';
+
+const config = defineConfig({
+  plugins: [
+    viteTsConfigPaths({
+      projects: ['./tsconfig.json'],
+    }),
+    devtools(),
+    tanstackStart(),
+    nitro({ noExternals: true }),
+    viteReact(),
+  ],
+  nitro: {
+    plugins: ['src/nitro/plugins/migrate.ts'],
+  },
+});
+
+export default config;
