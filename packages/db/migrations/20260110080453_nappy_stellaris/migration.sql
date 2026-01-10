34891c487fbb9a4bce845ac7223794e528c67758
Bryan <git@singee.me>
2026-01-10 15:06:10 +0700

    require not null for most fields of repos table

---
+++ packages/db/migrations/20260110080453_nappy_stellaris/migration.sql
@@ -0,0 +1,6 @@
+ALTER TABLE "repos" ALTER COLUMN "repo_name" SET NOT NULL;--> statement-breakpoint
+ALTER TABLE "repos" ALTER COLUMN "repo_details" SET NOT NULL;--> statement-breakpoint
+ALTER TABLE "repos" ALTER COLUMN "description" SET NOT NULL;--> statement-breakpoint
+ALTER TABLE "repos" ALTER COLUMN "initial_description" SET NOT NULL;--> statement-breakpoint
+ALTER TABLE "repos" ALTER COLUMN "starred_at" SET NOT NULL;--> statement-breakpoint
+ALTER TABLE "repos" ALTER COLUMN "description_updated_at" SET NOT NULL;
